24500e03242a18c07ebceeac3fd80f3d0fc4621a
jyun38 <jyun38@smith.edu>
2017-06-21 12:42:28 -0400

    flask connected

--- FlaskApp/flaskData.py
+++ FlaskApp/flaskData.py
@@ -3,14 +3,20 @@ from flask import Flask, request, redirect, render_template
 from flask_mysqldb import MySQL
 import sys
 
+# Create connection with MySQL
 mysql = MySQL()
+
+# Create Flask app
 app = Flask(__name__)
+
+# Set user, password, database, and host
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'dbswldud'
 app.config['MYSQL_DB'] = 'MentalHealth'
 app.config['MYSQL_HOST'] = 'localhost'
 mysql = MySQL(app)
 
+# Route for getting the result of a query
 @app.route('/')
 def index():
 	cur = mysql.connection.cursor()
@@ -18,36 +24,22 @@ def index():
 	rv = cur.fetchall()
 	return str(rv)
 
+# Route for form with the button 
 @app.route('/result', methods=['POST', 'GET'])
 def result():
 	return render_template('button.html')
-	# print("in function")
-	# print(request.form)
-	# print(request.form['click'])
-	# return redirect('/')
-	
-	# if request.form['clicked'] == "ON":
-	# 	print("yes")
-	# elif request.form['clicked'] == "OFF":
-	# 	print("no")
+
+# Route after the button is clicked
 @app.route('/afterbutton')
-def yes():
-	# print("YES", file=sys.stderr)
-	# return "YES"
-	#return redirect('/')
+def afterButton():
 	value = request.args.get('mybutton')
 	print(value, file=sys.stderr)
 	cur = mysql.connection.cursor()
-	# cur.execute('''SELECT * FROM MentalHealth.category_description WHERE description = ''' + str(value))
-	# # line = '''SELECT * FROM MentalHealth.category_description WHERE category_ID = 111'''
 	cur.execute('''SELECT * FROM MentalHealth.category_description WHERE description = %s''', (value,))
-	# query = "SELECT * FROM MentalHealth.category_description WHERE description = (?)"
-	# cur.execute("SELECT * FROM MentalHealth.category_description WHERE description = (?)", value)
 	rv = cur.fetchall()
 	return str(rv)
 
-
-
+# Main
 if __name__ == "__main__":
 	app.run(debug=True)
 
