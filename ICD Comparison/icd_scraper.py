6c0da924ac83040fb37e1115a9e480e21a729eb4
kpien <pienkelly@gmail.com>
2018-05-17 09:38:29 -0400

    Add partially scraped data & scrapers in progress

---
+++ ICD Comparison/icd_scraper.py
@@ -0,0 +1,22 @@
+#import requests
+from bs4 import BeautifulSoup
+#import pandas
+from html.parser import HTMLParser
+
+def main():
+	#of = open("icd_data.csv", "w")
+	soup = BeautifulSoup(open("icd_bluebook.html"), "html.parser")
+	#print(soup.prettify())
+##	names_codes_list = soup.find_all("p", {"class" : "p26"})
+##	for diagnosis in names_codes_list:
+##		stringified = str(diagnosis).split('<span class="s1">')[1].split('</b>)')[0].split('</span>')[0]
+##		if "<b>" in stringified:
+##                        stringified = stringified.split("<b>")[1]
+##                        if "</b>" in stringified:
+##                                stringified = stringified.split("</b>")[0]
+##		of.write(stringified + "\n")
+	diag_crit = soup.find_all("p", {"class" : "p1"})
+	for criterion in diag_crit:
+                print(criterion)
+
+main()
